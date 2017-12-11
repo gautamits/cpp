709b91c73968db46ac7345a0fdd7df8255fe23d2
gautamits <gautamamits95@gmail.com>
2017-12-11 11:21:58 +0530

    sort and reverse sort

---
+++ stl/sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+bool cmp(int a,int b){
+	return a>b;
+}
+int main(){
+	int n=20;
+	int arr[n];
+	for (int i=0;i<n;i++) arr[i]=rand()%100;
+	cout<<"array is\n";
+	for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+	cout<<endl;
+	cout<<"normal sorting "<<endl;
+	sort(arr,arr+n);
+	for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+	cout<<endl;
+	cout<<"reverse sorting\n";
+	sort(arr,arr+n,cmp);
+	for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+	cout<<endl;
+
+}
